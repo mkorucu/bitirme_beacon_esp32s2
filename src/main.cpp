f104b4f27319ae611e64001b9b6dc79f2300a992
mehmet soner korucu <mehmetsonerkorucu@hotmail.com>
2024-03-27 17:27:41 +0300

    lora sent lib done.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
 extern "C" {void app_main();}
 
-void app_main() {}
+#include <Lora_Service.h>
+
+void app_main()
+{
+    char buff[100];
+    Lora_Service lora_service(LORA_EN_PIN);
+
+    lora_service.init();
+    for(int i = 0; ; i++)
+    {
+        sprintf(buff, "selam %d", i);
+        lora_service.sent_data(buff);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
